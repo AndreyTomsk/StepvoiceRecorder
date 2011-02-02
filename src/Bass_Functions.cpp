c7f112dc39a528c35f37116570455c02ccc2183a
Andrew Firsov <andrew.firsov@gmail.com>
2011-02-02 21:08:29 +0000

    Vista loopback DSP updated to float samples.
    git-svn-id: file:///home/andrew/%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B/_from_dell/SVN_Repository_stepvoice/StepVoice/SvRecorder/trunk@185 95bd2046-0065-4c11-9034-03c053182b31

--- src/Bass_Functions.cpp
+++ src/Bass_Functions.cpp
@@ -8,32 +8,46 @@ namespace Bass
 //------------------------------------------------------------------------------
 float GetMaxPeakDB(HRECORD a_handle)
 {
-	//BASS_CHANNELINFO l_ci;
-	//BOOL l_result = BASS_ChannelGetInfo(a_handle, &l_ci);
-	//ASSERT(l_result);
-
-	//int l_max_possible = 0x7FFF; // 16-bit as default, 0xFFFF/2
-	//if (l_ci.flags & BASS_SAMPLE_8BITS)
-	//	l_max_possible = 0x7F; //0xFF/2
-	//else if (l_ci.flags & BASS_SAMPLE_FLOAT)
-	//	l_max_possible = 1;
-
-	DWORD l_ch_level = BASS_ChannelGetLevel(a_handle);
+	DWORD l_ch_level = BASS_ChannelGetLevel(a_handle); // 0..32768
 	ASSERT(l_ch_level != -1);
 
-	DWORD l_level = max(LOWORD(l_ch_level), HIWORD(l_ch_level)); // 0-32768=0x8000.
-	const DWORD l_max_possible = 0x8000;
-
-	return 20 * log10(float(l_level) / l_max_possible);
+	DWORD l_level = max(LOWORD(l_ch_level), HIWORD(l_ch_level));
+	return 20 * log10(float(l_level) / 32768);
 }
 
 //------------------------------------------------------------------------------
 void CALLBACK LoopbackStreamDSP(HDSP /*a_dsp_handle*/, DWORD /*a_channel*/,
-								void *a_buffer, DWORD a_length, void *a_user)
+								void *a_buffer, DWORD a_length_bytes, void *a_user)
 {
 	// Loopback is a decoding stream and have same parameters (freq, channels)
 	// as a main recording stream, for direct copying.
+	ASSERT(BASS_GetConfig(BASS_CONFIG_FLOATDSP) != -1); // float samples check
+	ASSERT (a_user != NULL);	
 
+	const DWORD TEMP_BUFFER_SIZE = 4096;
+	static float l_tmp_buffer[TEMP_BUFFER_SIZE] = {0.0};
+	HSTREAM l_src_stream = *((HSTREAM*)a_user);
+	DWORD l_copy_offset = 0;
+
+	while (a_length_bytes > 0)
+	{
+		DWORD l_copy_count = min(TEMP_BUFFER_SIZE * sizeof(float), a_length_bytes);
+		DWORD l_bytes_copied = BASS_ChannelGetData(l_src_stream, l_tmp_buffer,
+			l_copy_count | BASS_DATA_FLOAT);
+		
+		if (l_bytes_copied == -1)
+		{
+			//int l_debug = BASS_ErrorGetCode();
+			ASSERT(false);
+			return;
+		}
+
+		memcpy((BYTE*)a_buffer + l_copy_offset, l_tmp_buffer, l_bytes_copied);
+		a_length_bytes -= l_bytes_copied;
+		l_copy_offset += l_bytes_copied;
+	}
+
+/*
 	//1. Fill buffer with required length from Loopback stream (a_user)
 	const int BUFFER_LENGTH = 128 * 1024;
 	ASSERT(a_length <= BUFFER_LENGTH);
@@ -51,19 +65,19 @@ void CALLBACK LoopbackStreamDSP(HDSP /*a_dsp_handle*/, DWORD /*a_channel*/,
 	}
 	ASSERT(l_length == a_length);
 
+	//2. Replace data in the recording buffer (assuming we have float samples).
+	char* l_dst_buffer = (char*)a_buffer;
+	for (DWORD i = 0; i < a_length; i++)
+	l_dst_buffer[i] = l_src_buffer[i];
+*/
 	/*
 	//2. Mix data with destination buffer (assuming we have float samples).
 	float* l_dst_buffer = (float*)a_buffer;
 	float* l_src_buffer_float = (float*)l_src_buffer;
 
 	for (int i = 0, count = a_length / sizeof(float); i < count; i++)
-		l_dst_buffer[i] = max(-1.0, min((l_dst_buffer[i] + l_src_buffer_float[i]), 1.0));
+	l_dst_buffer[i] = max(-1.0, min((l_dst_buffer[i] + l_src_buffer_float[i]), 1.0));
 	*/
-
-	//2. Replace data in the recording buffer (assuming we have float samples).
-	char* l_dst_buffer = (char*)a_buffer;
-	for (DWORD i = 0; i < a_length; i++)
-	l_dst_buffer[i] = l_src_buffer[i];
 }
 
 //------------------------------------------------------------------------------
