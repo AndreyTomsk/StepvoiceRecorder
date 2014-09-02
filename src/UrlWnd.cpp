67ba88ca84ab30a975ddfc39854b195e1b62ab79
Andrew Firsov <andrew.firsov@gmail.com>
2014-09-02 06:03:46 +0000

    Module cleaned.
    git-svn-id: file:///home/andrew/%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B/_from_dell/SVN_Repository_stepvoice/StepVoice/SvRecorder/trunk@249 95bd2046-0065-4c11-9034-03c053182b31

--- src/UrlWnd.cpp
+++ src/UrlWnd.cpp
@@ -1,6 +1,3 @@
-// UrlWnd.cpp : implementation file
-//
-
 #include "stdafx.h"
 #include "mp3_recorder.h"
 #include "UrlWnd.h"
@@ -14,49 +11,44 @@ static char THIS_FILE[] = __FILE__;
 /////////////////////////////////////////////////////////////////////////////
 BEGIN_MESSAGE_MAP(CUrlWnd, CStatic)
 	//{{AFX_MSG_MAP(CUrlWnd)
-	ON_WM_LBUTTONDOWN()
 	ON_WM_CREATE()
+	ON_WM_LBUTTONDOWN()
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
+
 CUrlWnd::CUrlWnd()
 {
 }
+//---------------------------------------------------------------------------
 
 CUrlWnd::~CUrlWnd()
 {
 }
-
-/////////////////////////////////////////////////////////////////////////////
-/*
-void CUrlWnd::Init()
-{
-	SetClassLong(m_hWnd, GCL_HCURSOR, (LONG)LoadCursor (AfxGetInstanceHandle (), MAKEINTRESOURCE (IDC_HAND)));
-	GetWindowText(m_strUrl);
-}
-*/
+//---------------------------------------------------------------------------
 
 void CUrlWnd::SetUrl(CString pszUrl)
 {
 	m_strUrl = pszUrl;
 }
+//---------------------------------------------------------------------------
 
 int CUrlWnd::OnCreate(LPCREATESTRUCT lpCreateStruct) 
 {
 	if (CStatic::OnCreate(lpCreateStruct) == -1)
 		return -1;
 	
-	//Init();
-	SetClassLong(m_hWnd, GCL_HCURSOR, (LONG)LoadCursor (AfxGetInstanceHandle (),
-		MAKEINTRESOURCE(IDC_HAND)));
+	SetClassLong(m_hWnd, GCL_HCURSOR,
+		(LONG)LoadCursor(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDC_HAND)) );
 	
 	return 0;
 }
-
+//---------------------------------------------------------------------------
 
 void CUrlWnd::OnLButtonDown(UINT nFlags, CPoint point) 
 {
 	ShellExecute(::GetDesktopWindow(), "open", m_strUrl, NULL, NULL, SW_SHOW);
 	CStatic::OnLButtonDown(nFlags, point);
 }
+//---------------------------------------------------------------------------
--- src/UrlWnd.h
+++ src/UrlWnd.h
@@ -1,52 +1,32 @@
-#if !defined(AFX_URLWND_H__5A0A2462_64CF_4FF8_B7C6_A24BBC3EFF42__INCLUDED_)
-#define AFX_URLWND_H__5A0A2462_64CF_4FF8_B7C6_A24BBC3EFF42__INCLUDED_
-
-#if _MSC_VER > 1000
+#ifndef _URLWND_H
+#define _URLWND_H
 #pragma once
-#endif // _MSC_VER > 1000
-// UrlWnd.h : header file
-//
 
 /////////////////////////////////////////////////////////////////////////////
-// CUrlWnd window
 
 class CUrlWnd : public CStatic
 {
-// Construction
 public:
 	CUrlWnd();
+	virtual ~CUrlWnd();
 
-// Attributes
-public:
+	void SetUrl(CString pszUrl);
 
-// Operations
-public:
-
-// Overrides
 	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CUrlWnd)
 	//}}AFX_VIRTUAL
-
-// Implementation
-public:
-	//void Init();
-	void SetUrl(CString pszUrl);
-	virtual ~CUrlWnd();
-
 	// Generated message map functions
 protected:
-	CString m_strUrl;
 	//{{AFX_MSG(CUrlWnd)
-	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 	//}}AFX_MSG
-
 	DECLARE_MESSAGE_MAP()
+
+private:
+	CString m_strUrl;
 };
 
 /////////////////////////////////////////////////////////////////////////////
 
-//{{AFX_INSERT_LOCATION}}
-// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
-
-#endif // !defined(AFX_URLWND_H__5A0A2462_64CF_4FF8_B7C6_A24BBC3EFF42__INCLUDED_)
+#endif // _URLWND_H
