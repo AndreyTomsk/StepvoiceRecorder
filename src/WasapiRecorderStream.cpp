3a9eaa4b7f5916d668943f570391f6b3b9e6f42f
Andrew Firsov <andrew.firsov@gmail.com>
2015-05-04 12:53:56 +0000

    CWasapiRecorderStream rewritten using code from BassVistaLoopback. Motivation: update HSTREAM to pull data from WASAPI engine - better recording without glitches.
    git-svn-id: file:///home/andrew/%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B/_from_dell/SVN_Repository_stepvoice/StepVoice/SvRecorder/trunk@328 95bd2046-0065-4c11-9034-03c053182b31

--- src/WasapiRecorderStream.cpp
+++ src/WasapiRecorderStream.cpp
@@ -1,9 +1,9 @@
 #include "stdafx.h"
-#include <bass.h>
 #include <basswasapi.h>
 #include "WasapiRecorderStream.h"
-#include "WasapiHelpers.h"
+#include "WasapiHelpers.h" //for GetActiveDevice
 #include "Debug.h"
+#include "common.h" //for EIF
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -11,112 +11,213 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
+#define MFTIMES_PER_MILLISEC  10000
+
 /////////////////////////////////////////////////////////////////////////////
 
-CWasapiRecorderStream::CWasapiRecorderStream(int device, DWORD freq, DWORD chans)
+CWasapiRecorderStream::CWasapiRecorderStream(int device)
 	:m_deviceID(device)
-	,m_isStarted(false)
 	,m_hStream(0)
+	,m_wfx(NULL)
+	,m_src_offset(0)
+	,m_buffer_delay(false)
+	,m_buffer_size(0)
 {
-	//BASS_Init for no-sound device and BASS_Free must be called from the
-	//multi-recorder.
-	
-	if (!WasapiHelpers::GetDeviceActualData(device, freq, chans, m_actualFreq, m_actualChans))
-		return;
+	HRESULT hr;
 
-	//m_hStream = BASS_StreamCreate(m_actualFreq, m_actualChans,
-	//	BASS_SAMPLE_FLOAT|BASS_STREAM_DECODE, STREAMPROC_PUSH, NULL);
-	//ASSERT(m_hStream != 0);
+	// Taking device, based on driver name, not ID (BASS enumeration doesn't
+	// equal with WASAPI's). Again, real device IDs starts from 1 in BASS.
+	BASS_WASAPI_DEVICEINFO info;
+	BOOL result = BASS_WASAPI_GetDeviceInfo(device, &info);
+	ASSERT(result);
+	EIF(WasapiHelpers::GetActiveDevice(CString(info.id), &m_audio_client));
 
-	BOOL result = BASS_WASAPI_Init(device, m_actualFreq, m_actualChans,
-		BASS_WASAPI_AUTOFORMAT, 0.5, 0,	OutputProc, this);
-	ASSERT(result);
-	result = BASS_WASAPI_Start();
-	ASSERT(result);
+	const DWORD streamFlags = (info.flags & BASS_DEVICE_LOOPBACK) ? AUDCLNT_STREAMFLAGS_LOOPBACK : 0;
+	const REFERENCE_TIME bufferDuration = 500 * MFTIMES_PER_MILLISEC;
+
+	EIF(m_audio_client->GetMixFormat(&m_wfx));
+	EIF(m_audio_client->Initialize(AUDCLNT_SHAREMODE_SHARED, streamFlags, bufferDuration, 0, m_wfx, NULL));
+	EIF(m_audio_client->GetBufferSize(&m_buffer_size)); //in frames
+
+	EIF(m_audio_client->GetService(__uuidof(IAudioCaptureClient),
+		(void**)&m_capture_client));
+	EIF(m_audio_client->Start());
+
+	m_hStream = BASS_StreamCreate(
+		GetActualFrequency(),
+		GetActualChannelCount(),
+		BASS_SAMPLE_FLOAT | BASS_STREAM_DECODE,
+		StreamProc,
+		this);
+
+Exit:
+	return;
 }
 //---------------------------------------------------------------------------
 
 CWasapiRecorderStream::~CWasapiRecorderStream()
 {
-	CMyLock lock(m_sync_object);
+	if (m_audio_client)
+	{
+		HRESULT hr = m_audio_client->Stop();
+		ASSERT(!FAILED(hr));
 
-	WasapiHelpers::CurrentThreadDevice temp(m_deviceID);
-	BASS_WASAPI_Stop(TRUE /*reset device buffer*/);
-	BASS_WASAPI_Free();
-
-	BASS_StreamFree(m_hStream);
-	m_hStream = 0;
-}
-//---------------------------------------------------------------------------
-
-int CWasapiRecorderStream::GetDeviceID() const
-{
-	return m_deviceID;
+		BASS_StreamFree(m_hStream);
+		CoTaskMemFree(m_wfx);
+	}
 }
 //---------------------------------------------------------------------------
 
 DWORD CWasapiRecorderStream::GetActualFrequency() const
 {
-	return m_actualFreq;
+	return (m_wfx != NULL) ? m_wfx->nSamplesPerSec : 0;
 }
 //---------------------------------------------------------------------------
 
 DWORD CWasapiRecorderStream::GetActualChannelCount() const
 {
-	return m_actualChans;
+	return (m_wfx != NULL) ? m_wfx->nChannels : 0;
 }
 //---------------------------------------------------------------------------
 
 HSTREAM CWasapiRecorderStream::GetStreamHandle() const
 {
-	if (!m_hStream)
-	{
-		m_hStream = BASS_StreamCreate(m_actualFreq, m_actualChans,
-			BASS_SAMPLE_FLOAT|BASS_STREAM_DECODE, STREAMPROC_PUSH, NULL);
-		ASSERT(m_hStream != 0);
-	}
 	return m_hStream;
-}
-//---------------------------------------------------------------------------
-
-void CWasapiRecorderStream::SetStarted(bool isStarted)
-{
-	CMyLock lock(m_sync_object);
-	m_isStarted = isStarted;
 }
 //---------------------------------------------------------------------------
 
 float CWasapiRecorderStream::GetVolume() const
 {
-	WasapiHelpers::CurrentThreadDevice temp(m_deviceID);
-	return BASS_WASAPI_GetVolume(BASS_WASAPI_CURVE_WINDOWS);
+	CComPtr<IChannelAudioVolume> channelVolume;
+	float resultVolume = 0;
+
+	HRESULT hr;
+	EIF(m_audio_client->GetService(__uuidof(IChannelAudioVolume), (void**)&channelVolume));
+	EIF(channelVolume->GetChannelVolume(0, &resultVolume));
+
+Exit:
+	return resultVolume;
 }
 //---------------------------------------------------------------------------
 
 BOOL CWasapiRecorderStream::SetVolume(float volume)
 {
-	WasapiHelpers::CurrentThreadDevice temp(m_deviceID);
-	return BASS_WASAPI_SetVolume(BASS_WASAPI_CURVE_WINDOWS, volume);
+	CComPtr<IChannelAudioVolume> channelVolume;
+	UINT32 channelCount = 0;
+	float* newVolumes = NULL;
+
+	HRESULT hr;
+	EIF(m_audio_client->GetService(__uuidof(IChannelAudioVolume), (void**)&channelVolume));
+	EIF(channelVolume->GetChannelCount(&channelCount));
+
+	ASSERT(channelCount > 0);
+	newVolumes = new float[channelCount];
+	for (unsigned i = 0; i < channelCount; i++)
+		newVolumes[i] = volume;
+
+	EIF(channelVolume->SetAllVolumes(channelCount, newVolumes, NULL));
+
+Exit:
+	SAFE_DELETE_ARRAY(newVolumes);
+	return hr == S_OK;
 }
 //---------------------------------------------------------------------------
 
 float CWasapiRecorderStream::GetPeakLevel(int channel) const
 {
-	const bool isMono = (m_actualChans == 1);
+	//TODO: rewrite without using BASS wasapi (see Get/SetVolume).
+	const bool isMono = (GetActualChannelCount() == 1);
 	const float level = BASS_WASAPI_GetDeviceLevel(m_deviceID, isMono ? 0 : channel);
-	ASSERT(level != -1);
+	//ASSERT(level >= 0);
 	return level;
 }
 //---------------------------------------------------------------------------
 
-DWORD CALLBACK CWasapiRecorderStream::OutputProc(void* buffer, DWORD lengthBytes, void* user)
+DWORD CALLBACK CWasapiRecorderStream::StreamProc(HSTREAM,
+	void* a_buffer, DWORD a_length, void* a_user)
 {
-	CWasapiRecorderStream* recorder = static_cast<CWasapiRecorderStream*>(user);
+	//TODO: refactor, looks overcomplicated.
 
-	CMyLock lock(recorder->m_sync_object);
-	if (recorder->m_isStarted && recorder->m_hStream != 0)
-		BASS_StreamPutData(recorder->m_hStream, buffer, lengthBytes);
+	CWasapiRecorderStream* l_this = static_cast<CWasapiRecorderStream *>(a_user);
+	ASSERT(l_this);
 
-	return 1;
+	HRESULT hr;
+
+	// Applying sound delay for 0.1 sec (currently).
+	{
+		UINT32 paddingFrameCount;
+		hr = l_this->m_audio_client->GetCurrentPadding(&paddingFrameCount);
+		if (!paddingFrameCount || (paddingFrameCount < l_this->m_buffer_size/4 && !l_this->m_buffer_delay))
+		{
+			//if (addingFrameCount)
+			//	::OutputDebugString(__FUNCTION__ " ::Delaying (wait 1/4 buffer)");
+			//else
+			//	::OutputDebugString(__FUNCTION__ " ::Delaying (no frames in buffer");
+
+			l_this->m_buffer_delay = false;
+			ZeroMemory(a_buffer, a_length);
+			return a_length;
+		}
+		else
+			l_this->m_buffer_delay = true;
+	}
+
+	BYTE*  l_dst_buffer = (BYTE*)a_buffer;
+	UINT32 l_dst_length = a_length;
+
+	// NOTE: we fill the required buffer completely
+	while (l_dst_length)
+	{
+		// 1. Getting new chunk
+		// 2. If we don't have any sounds, then just making the remaining part
+		//      of the buffer silent and exit
+		// 3. Counting data length for copying
+		// 4. Filling destination buffer
+		// 5. Updating variables
+		// 6. If l_dst_length, then repeat
+
+		BYTE*   l_src_buffer = NULL;
+		DWORD   l_src_flags = 0;
+		UINT32  l_src_frames_available = 0;
+		UINT32& l_src_offset = l_this->m_src_offset;
+
+		hr = l_this->m_capture_client->GetBuffer(&l_src_buffer,
+			&l_src_frames_available, &l_src_flags, NULL, NULL);
+
+		if (!l_src_frames_available)
+		{
+			if (l_dst_length == a_length)
+				::OutputDebugString(__FUNCTION__ " ::Empty buffer");
+			else
+				::OutputDebugString(__FUNCTION__ " ::Empty buffer (partially filled)");
+
+			ZeroMemory(l_dst_buffer, l_dst_length);
+			hr = l_this->m_capture_client->ReleaseBuffer(l_src_frames_available);
+			// Return filled length if no more frames.
+			return (l_dst_length == a_length) ? a_length : a_length - l_dst_length;
+		}
+
+		UINT32 l_src_bytes_available = l_src_frames_available * l_this->m_wfx->nBlockAlign;
+
+		if (l_src_bytes_available > a_length)
+			::OutputDebugString(__FUNCTION__ " :: TOO MANY FRAMES!");
+
+		UINT32 l_bytes_4copy = min(l_dst_length, l_src_bytes_available - l_src_offset);
+
+		memcpy(l_dst_buffer, l_src_buffer + l_src_offset, l_bytes_4copy);
+
+		l_dst_length -= l_bytes_4copy;
+		l_dst_buffer += l_bytes_4copy;
+		l_src_offset += l_bytes_4copy;
+
+		ASSERT(l_src_offset <= l_src_bytes_available);
+		if (l_src_offset == l_src_bytes_available)
+			l_src_offset = 0;	// clearing offset if all copied
+
+		// Asking for same chunk (0) if it wasn't fully copied
+		hr = l_this->m_capture_client->ReleaseBuffer(
+			(l_src_offset) ? 0 : l_src_frames_available);
+	}
+	return a_length;
 }
 //---------------------------------------------------------------------------
--- src/WasapiRecorderStream.h
+++ src/WasapiRecorderStream.h
@@ -2,36 +2,29 @@
 #define WASAPI_RECORDER_STREAM_H
 #pragma once
 
-#include "SyncObjects.h"
+#include <Audioclient.h> //for IAudioClient, IAudioCaptureClient
 
 /////////////////////////////////////////////////////////////////////////////
 //Ok, idea is currently to provide a HSTREAM handle for multi-recorder.
-//WASAPI recorder starts recording immediately, playback/pause should be
-//controlled using the HSTREAM.
-
-//BASS_Init for a no-sound device (+BASS_Free) must be called from the
-//multi-recorder, before creating the CWasapiRecorderStream objects.
+//It is a decoding stream (requirement from BASSMix), so no playback/pause
+//control needed. Mixer just reads data from stream.
 
 //Action sequence:
 //  1. Create RecorderStream object;
 //  2. Retrieve actual frequency and channels;
 //  3. Get stream handle;
-//  4. Set started flag to put a recording data to stream;
 
 class CWasapiRecorderStream
 {
 public:
 	typedef DWORD HSTREAM; //Sample stream handle (from bass.h).
 
-	CWasapiRecorderStream(int device, DWORD freq, DWORD chans);
+	CWasapiRecorderStream(int device);
 	~CWasapiRecorderStream();
 
-	int GetDeviceID() const;
 	DWORD GetActualFrequency() const;
 	DWORD GetActualChannelCount() const;
-
 	HSTREAM GetStreamHandle() const;
-	void SetStarted(bool isStarted);
 
 	float GetVolume() const;
 	BOOL  SetVolume(float volume); //0..1
@@ -39,15 +32,20 @@ public:
 	float GetPeakLevel(int channel) const; //0 = first channel, -1 = all channels
 
 private:
-	static DWORD CALLBACK OutputProc(void* buffer, DWORD lengthBytes, void* user);
+	static DWORD CALLBACK StreamProc(HSTREAM handle,
+		void* dstBuffer, DWORD dstLengthBytes, void* user);
 
-	mutable HSTREAM m_hStream;
+private:
+	CComPtr<IAudioClient> m_audio_client;
+	CComPtr<IAudioCaptureClient> m_capture_client;
 
 	int m_deviceID;
-	DWORD m_actualFreq;
-	DWORD m_actualChans;
-	bool m_isStarted;
-	CMyCriticalSection m_sync_object;
+	HSTREAM m_hStream;
+	WAVEFORMATEX* m_wfx;
+	
+	UINT32 m_src_offset;
+	bool m_buffer_delay;
+	UINT32 m_buffer_size;
 };
 
 /////////////////////////////////////////////////////////////////////////////
