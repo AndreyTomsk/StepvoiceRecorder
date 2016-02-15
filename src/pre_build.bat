76e109fa809e346b2742264c97dbbeb56ba0b3fc
Andrew Firsov <andrew.firsov@gmail.com>
2016-02-15 20:31:29 +0600

    Version generation updated to git.

--- src/pre_build.bat
+++ src/pre_build.bat
@@ -4,6 +4,19 @@ rem (Not needed) Preparing build date string for version file (for Russian)
 rem for /f "delims=. tokens=1,2,3" %%i in ("%DATE%") do set BUILD_DATE=%%k%%j%%i
 rem echo %BUILD_DATE:~2,6%
 
-rem Creating version.h containing version information based on svn rev. number
-set PATH=%PATH%;%PROGRAMFILES%\TortoiseSVN\bin\
-SubWCRev.exe ..\ version.in version.h
+rem Creating version.h containing version information based on git rev. number
+for /f "delims=" %%a in ('git rev-list --all --count') do @set RevNumber=%%a
+
+set VersionDots=1.9.1.%RevNumber%
+set VersionCommas=%VersionDots:.=,%
+
+echo #pragma once				> version.h
+echo.						>>version.h
+echo #define FILEVER    %VersionCommas%		>>version.h
+echo #define PRODUCTVER %VersionCommas%		>>version.h
+echo.						>>version.h
+echo #ifdef UNICODE				>>version.h
+echo   #define STRFILEVER L"%VersionDots%\0"	>>version.h
+echo #else					>>version.h
+echo   #define STRFILEVER  "%VersionDots%\0"	>>version.h
+echo #endif					>>version.h
